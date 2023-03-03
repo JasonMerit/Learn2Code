0189290abd0087f450f6ccf155c2647f2acbd323
Jason Aron Merit <76186383+JasonMerit@users.noreply.github.com>
2023-03-03 14:31:12 +0100

    Game class and started test script

--- Main.py
+++ Main.py
@@ -1,30 +1,51 @@
 # Kryds og bolle
 
-board = [[1, 2, 0], [0, 0, 0], [1, 0, 0]]
-letters = ["a ", "b ", "c "]
 
-valid_first = ["a", "b", "c"]
-valid_second = ["0", "1", "2"]
+class Game:
+    grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    letters = ["a ", "b ", "c "]
 
-# letter / number
-def show():
-    print("   0  1  2 ")
-    for i, b in enumerate(board):
-        row = letters[i]
-        for v in b:
-            row += " x " if v == 1 else " o " if v == 2 else "[ ]"
-        print(row)
-    print()
+    valid_first = ["a", "b", "c"]
+    valid_second = ["0", "1", "2"]
+    letter2num = {"a": 0, "b": 1, "c": 2}
 
-show()
+    player = 1 # 0 = O, 1 = X
+    
+    def show(self):
+        print("   0  1  2 ")
+        for i, b in enumerate(self.grid):
+            row = self.letters[i]
+            for v in b:
+                row += " x " if v == 1 else " o " if v == 2 else "[ ]"
+            print(row)
+        print()
 
-def get_input():
-    print("X's turn - Type coordinate <xy>")
-    user = input(">")
-    while len(user) != 2 or user[0] not in valid_first or user[1] not in valid_second:
-        print("Invalid input - Type coordinate <xy>")
-        user = input(">")
-    return user
+    def get_input(self):
+        """Get input from user and return it as a tuple (str, str)"""
+        print("X's turn - Type coordinate >AN")
+        user = ""
+        while True:
+            user = input(">")
+            if user in ["q", "quit", "exit"]:
+                quit()
+            if len(user) == 2 and user[0] in self.valid_first and user[1] in self.valid_second:
+                return user[0], user[1]
+            print("Invalid input - Type coordinate >AN")
 
-x, y = get_input()
-print("GREAT SUCCESS")
+
+    def play(self):    
+        y, x = self.get_input()  # TODO: Flip y and x or forget int(y)
+        self.step(y, x)
+        self.show()
+        
+    
+    def step(self, y, x):
+        """Place a mark on the grid at coordinate (y, x) (str, str) and switch player"""
+        self.grid[self.letter2num[y]][int(x)] = self.player % 2
+        self.player += 1
+
+if __name__ == "__main__":
+    game = Game()
+    while True:
+        game.play()
+
---
+++ errors.py
@@ -0,0 +1,19 @@
+
+# Pointer error - Copying objects results in multiple references to the same object
+# Take away - Doesn't need to be sexy. 
+A = [[0]*3]*3
+
+A[1][1] = 1
+for a in A:
+    print(a)
+
+
+
+# Debugger - Go to Debug Console after running debugger. Then type variables there. 
+# Syntactic sugar - Fancy coding
+
+
+# ------- Story -------
+
+# Write test functions instead of playing out every time
+# - Create class for game
